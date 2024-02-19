8082fd9d22ff4a87f218049a01a357c81c96566a
uhyunjin <uhyeonjin0619@gmail.com>
2024-02-19 22:44:15 +0900

    setter 호출하지 않고 property 태그로 처리하기(setter호출함)

--- src/main/java/com/fastcampus/ch3/SpringDITest.java
+++ src/main/java/com/fastcampus/ch3/SpringDITest.java
@@ -54,10 +54,10 @@ public class SpringDITest {
         Engine engine2 = (Engine) ac.getBean(Engine.class); //bytype
         Door door = (Door) ac.getBean("door");
 
-        car.setColor("red");
-        car.setOil(100);
-        car.setEngine(engine);
-        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
+//        car.setColor("red");
+//        car.setOil(100);
+//        car.setEngine(engine);
+//        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
         // car객체의 멤버 초기화
         // door는 항상 다른 객체가 생성되어야 하므로 prototype으로 설정
 

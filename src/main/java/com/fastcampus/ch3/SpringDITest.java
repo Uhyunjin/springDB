2afdfbcd968c28d02d5eca60523d07cd37fb18c3
uhyunjin <uhyeonjin0619@gmail.com>
2024-02-19 22:09:55 +0900

    application context : bytype

--- src/main/java/com/fastcampus/ch3/SpringDITest.java
+++ src/main/java/com/fastcampus/ch3/SpringDITest.java
@@ -11,11 +11,13 @@ class Door{}
 public class SpringDITest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
-        Car car = (Car)ac.getBean("car"); // byname
-        Engine engine = (Engine) ac.getBean("engine"); //byname
+        //Car car = (Car)ac.getBean("car"); // byname
+        Car car2 = (Car)ac.getBean(Car.class); // bytype
+        //Engine engine = (Engine) ac.getBean("engine"); //byname
+        Engine engine2 = (Engine) ac.getBean(Engine.class); //bytype
         Door door = (Door) ac.getBean("door");
-        System.out.println("car = " + car);
-        System.out.println("engine = " + engine);
+        System.out.println("car = " + car2);
+        System.out.println("engine = " + engine2);
         System.out.println("door = " + door);
     }
 }
